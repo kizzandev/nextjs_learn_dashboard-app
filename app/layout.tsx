382e1c87903fed22b015318b389db810fd25e107
kevinzanzi <69643444+kevinzanzi@users.noreply.github.com>
2024-02-26 19:00:42 -0300

    Add Image component to display hero images

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`} >{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
