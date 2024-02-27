3078e9059e7215740241f06fdfc56f44260dcf10
kevinzanzi <69643444+kevinzanzi@users.noreply.github.com>
2024-02-26 23:29:35 -0300

    Add search functionality and pagination to invoices page

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,7 +2,42 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
+//
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+
+// Prevent multiple calls to the database by using a debounce
+import { useDebouncedCallback } from 'use-debounce';
+
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching... ${term}`);
+    
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
+  /*function handleSearch(term: string) {
+    console.log(`Searching... ${term}`);
+
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }*/
+
+  // We use "defaultValue" instead of "value" because we are not using a state but rather the URL
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +46,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
