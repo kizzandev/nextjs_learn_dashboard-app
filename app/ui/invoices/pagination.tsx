da8f239f2c065611293979f1b64c74241591cc84
kevinzanzi <69643444+kevinzanzi@users.noreply.github.com>
2024-02-27 16:23:48 -0300

    formatting

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -15,7 +15,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
     const params = new URLSearchParams(searchParams);
     params.set('page', pageNumber.toString());
     return `${pathname}?${params.toString()}`;
-  }
+  };
 
   const allPages = generatePagination(currentPage, totalPages);
 
