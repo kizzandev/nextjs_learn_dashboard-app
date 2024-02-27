3daccc64ad236b9825d3d46d8894722adf811893
kevinzanzi <69643444+kevinzanzi@users.noreply.github.com>
2024-02-27 17:23:08 -0300

    Fix formatting in not-found.tsx and error.tsx

--- app/dashboard/invoices/[id]/edit/not-found.tsx
+++ app/dashboard/invoices/[id]/edit/not-found.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { FaceFrownIcon } from '@heroicons/react/24/outline';
- 
+
 export default function NotFound() {
   return (
     <main className="flex h-full flex-col items-center justify-center gap-2">
