38b0a84df68c3ae067922f743fe0a27f21028ab2
kevinzanzi <69643444+kevinzanzi@users.noreply.github.com>
2024-02-27 23:36:18 -0300

    Add customers page and table components

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -4,6 +4,22 @@ export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+import Table from '@/app/ui/customers/table';
+import { CustomersTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+  return (
+    <Suspense fallback={<CustomersTableSkeleton />}>
+      <Table query={query} />
+    </Suspense>
+  );
 }
--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,16 +1,11 @@
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
-import {
-  CustomersTableType,
-  FormattedCustomersTable,
-} from '@/app/lib/definitions';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 
-export default async function CustomersTable({
-  customers,
-}: {
-  customers: FormattedCustomersTable[];
-}) {
+export default async function CustomersTable({query}: {query: string;}) {
+  const customers = await fetchFilteredCustomers(query);
+
   return (
     <div className="w-full">
       <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
